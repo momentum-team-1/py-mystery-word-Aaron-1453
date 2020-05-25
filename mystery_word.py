17812651bd5166fe6e51fa62fd79a25a0d70765d
AaronOBrien1453 <aatobrien@gmail.com>
2020-05-24 21:57:29 -0400

    only need to add some user friendly features and fix some bugs

--- mystery_word.py
+++ mystery_word.py
@@ -1,22 +1,37 @@
 ##open file##
 
 import random
+
+
+def get_difficulty():
+    word = ""
+    difficulty = input(
+        "Please select difficulty level, type e for easy, n for normal, and h for hard ")
+    if difficulty == "e":
+        word = random.choice(easy_list)
+    elif difficulty == "n":
+        word = random.choice(normal_list)
+    elif difficulty == "h":
+        word = random.choice(hard_list)
+    return word
+
+
+# if __name__ == "__main__":
+
 file = open("words.txt", "r")
 text = file.read().split()
 ##get a random word, turn it into list - seperate letters - ##
 
-item = random.choice(text)
-word = list(item)
-print(word)
+# item = random.choice(text)
+# word = list(item)
+# print(word)
 
 ##hidden is the array of underscores that the word shows up as, you append the underscore to however many letters there are in word, which was defined above and is that random word from the list##
 
-hidden = []
-for letter in word:
-    hidden.append("_")
 
 attempts = 0
 max_attempts = 4
+
 
 easy_list = [
     word.upper()
@@ -37,39 +52,37 @@ hard_list = [
 ]
 
 
-def get_difficulty():
-    word = ""
-    difficulty = input(
-        "Please select difficulty level, type e for easy, n for normal, and h for hard ")
-    if difficulty == "e":
-        word = random.choice(easy_list)
-    elif difficulty == "n":
-        word = random.choice(normal_list)
-    elif difficulty == "h":
-        word = random.choice(hard_list)
-    return word
+word = (get_difficulty())
+# make word capitalized
+word = list(word.upper())
 
 
-if __name__ == "__main__":
-    word = (get_difficulty())
+hidden = []
+for letter in word:
+    hidden.append("_")
+
+
 print(f"The mystery word is {len(word)} characters long.")
-print(word)
-
+print("".join(word))
 
 ## loop till the player has won or lost##
 is_game_over = False
 while not is_game_over:
+
     # display current board, guessed letters, and attempts remaining
     # just makes the underscores into a string
     hidden_string = " ".join(hidden)
     print(f"You have {max_attempts - attempts} attempts remaining")
-    print(f'The current word is: {hidden_string}')
+    # print(f'The current word is: {hidden_string}')
     # ask for a guess --character-- and if they guess correctly show all matched letters and print message
 
     guess = input("Please guess a letter: ")
     correct_guesses = []
     wrong_guesses = []
     statement = ''
+
+    # make the guess capitalized
+    guess = guess.upper()
 
     if guess in word:
         # Iterate over string with index using range(), string being the letters of the word
@@ -80,12 +93,12 @@ while not is_game_over:
                 hidden[i] = word[i]
                 word[i] = "_"
         print(f'nice guess! "{guess}" is in the word')
-        print(hidden)
-        print(word)
+        print("".join(hidden))
+        # print("".join(word))
     elif guess not in word:
         print(f'Whoops! It looks like "{guess}" is not in this word')
         attempts += 1
-        print(word)
+        # print(word)
     # getting difficulty and random word##
     # running out of underscores has to end the game
     if (all('_' == char for char in word)):  # i don't see why != isn't used here
